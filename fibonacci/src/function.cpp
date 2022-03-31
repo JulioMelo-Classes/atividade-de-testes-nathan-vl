0e3c9e69330bc3e9a224d3b02b64ddb5309920de
Nathãn Vitor de Lima <nathan2001lima@gmail.com>
2022-03-31 17:55:22 -0300

    Altera assinatura da função de unsigned int para int

--- fibonacci/src/function.cpp
+++ fibonacci/src/function.cpp
@@ -1,12 +1,12 @@
 #include "function.h"
 
-std::vector<unsigned int> fib_below_n( unsigned int n )
+std::vector<int> fib_below_n( unsigned int n )
 {
     if (n < 2) {
-        return std::vector<unsigned int>{};
+        return std::vector<int>{};
     }
 
-    std::vector<unsigned int> result = {1, 1};
+    std::vector<int> result = {1, 1};
 
     while (result[result.size() - 1] < n) {
         auto vector_size = result.size();
--- fibonacci/src/function.h
+++ fibonacci/src/function.h
@@ -3,6 +3,6 @@
 
 #include <vector>
 
-std::vector<unsigned int> fib_below_n( unsigned int n );
+std::vector<int> fib_below_n( unsigned int n );
 
 #endif
