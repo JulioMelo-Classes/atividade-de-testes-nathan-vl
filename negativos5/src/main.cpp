73dcd463b9ca20c1d3a0150d55c12b8d55c162c8
Nathãn Vitor de Lima <nathan2001lima@gmail.com>
2022-03-31 18:13:42 -0300

    negativos5: adiciona código inicial

--- negativos5/src/main.cpp
+++ negativos5/src/main.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
-using std::cin;
-using std::cout;
+#include <array>
+#include <string>
 
-const int SIZE = 5; // input size.
+const int SIZE = 5;
 
 int main(void)
 {
-    // TODO: Adicione aqui seu código.
+    int total = 0;
+    for (auto i = 0; i < SIZE; ++i) {
+        std::string input;
+        std::cin >> input;
+
+        total += input[0] == '-';
+    }
+
+    std::cout << total << std::endl;
 
     return 0;
 }
